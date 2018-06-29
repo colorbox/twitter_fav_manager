96cae9574bb8c4000adc5864827bcfedff6bef7a
colorbox <colorbox222@gmail.com>
2018-06-29 19:39:18 +0900

    Update model relation

--- app/models/favorited_tweet.rb
+++ app/models/favorited_tweet.rb
@@ -1,2 +1,5 @@
 class FavoritedTweet < ApplicationRecord
+  belongs_to :tweet_owner
+  belongs_to :user
+  has_one :tweet
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ApplicationRecord
+  has_many :tweets, through: :favorited_tweets
 end
