ab677873f795436941cf03169eacc34b66432b24
colorbox <colorbox222@gmail.com>
2018-07-03 21:38:53 +0900

    Add fetched status to favorited_user model

--- app/models/favorited_user.rb
+++ app/models/favorited_user.rb
@@ -1,4 +1,6 @@
 class FavoritedUser < ApplicationRecord
   belongs_to :user
   belongs_to :tweet_owner
+
+  enum fetched_status: {not_fetched: 0, fetched: 1}
 end
--- db/migrate/20180701053901_create_favorited_users.rb
+++ db/migrate/20180701053901_create_favorited_users.rb
@@ -3,6 +3,7 @@ class CreateFavoritedUsers < ActiveRecord::Migration[5.2]
     create_table :favorited_users do |t|
       t.references :users
       t.references :tweet_owners
+      t.integer :fetch_status, default: 0, null: false, limit: 1
 
       t.timestamps
     end
