1ab6282ab00f418749aaad81743d97cc10bd352e
colorbox <colorbox222@gmail.com>
2018-07-16 03:28:31 +0900

    Add specific user favorites fetch association

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
 
   has_many :favorited_owners
   has_many :tweet_owners, through: :favorited_owners
+  has_many :timeline_not_fetched_tweet_owners, -> {where('favorited_owners.fetched_status = ?', FavoritedOwner.fetched_statuses[:not_fetched])}, through: :favorited_owners, source: :tweet_owner
 end
