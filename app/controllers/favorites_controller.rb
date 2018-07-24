62b509bcb3a77ecf88d65a466c4e82b0cc1aeead
colorbox <colorbox222@gmail.com>
2018-07-24 21:37:47 +0900

    Add kaminari gem

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,5 +1,5 @@
 class FavoritesController < ApplicationController
   def index
-    @tweets = current_user.tweets.limit(3) if current_user
+    @tweets = current_user.tweets.page(params[:page]) if current_user
   end
 end
