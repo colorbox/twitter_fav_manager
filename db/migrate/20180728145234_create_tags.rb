86ad182885e3be760dcb8aad3bee5a9b820958f3
colorbox <colorbox222@gmail.com>
2018-07-29 23:14:30 +0900

    Update tag model

--- db/migrate/20180728145234_create_tags.rb
+++ db/migrate/20180728145234_create_tags.rb
@@ -1,8 +1,12 @@
 class CreateTags < ActiveRecord::Migration[5.2]
   def change
     create_table :tags do |t|
+      t.references :user, null: false
+      t.references :favorited_tweet, null: false
 
       t.timestamps
+
+      t.index [:user_id, :favorited_tweet_id]
     end
   end
 end
