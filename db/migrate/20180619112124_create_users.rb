f43063059682afacb205266b7aa9ad289d12a8f2
colorbox <colorbox222@gmail.com>
2018-07-31 00:08:33 +0900

    wip for investigating with_options in migration file

--- db/migrate/20180619112124_create_users.rb
+++ db/migrate/20180619112124_create_users.rb
@@ -1,11 +1,22 @@
+require 'active_support/core_ext/object/with_options'
+
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :twitter_identifier, null: false
-      t.string :screen_name, null: false
-      t.string :name, null: false
-      t.string :access_token, null: false
-      t.string :access_token_secret, null: false
+      debugger
+      with_options null: false do
+        t.string :twitter_identifier
+        t.string :screen_name
+        t.string :name
+        t.string :access_token
+        t.string :access_token_secret
+
+      end
+      # t.string :twitter_identifier, null: false
+      # t.string :screen_name, null: false
+      # t.string :name, null: false
+      # t.string :access_token, null: false
+      # t.string :access_token_secret, null: false
 
       t.timestamps
     end
