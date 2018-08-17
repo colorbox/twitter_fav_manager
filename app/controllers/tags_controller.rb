92b9791b8aeefc68fa15ddffc000685a415b3188
colorbox <colorbox222@gmail.com>
2018-08-17 23:53:34 +0900

    Fix tag and favorited tweet association duprecation bug

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,7 +2,8 @@ class TagsController < ApplicationController
   def create
     tag = Tag.find_or_create_by!(title: tag_param[:title])
     favorited_tweet = FavoritedTweet.find(params[:favorite_id])
-    tag.favorited_tweets << favorited_tweet
+
+    tag.favorited_tweets << favorited_tweet if tag.favorited_tweets.exclude?(favorited_tweet)
 
     redirect_back(fallback_location: root_path)
   end
