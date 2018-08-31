3a8a0f434e0a45cebdc4c5595f51d38bb7c7e2a5
colorbox <colorbox222@gmail.com>
2018-08-31 23:58:46 +0900

    Reset db schema

--- db/schema.rb
+++ db/schema.rb
@@ -15,17 +15,6 @@ ActiveRecord::Schema.define(version: 2018_07_28_145234) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "favorited_owners", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "tweet_owner_id", null: false
-    t.integer "fetched_status", limit: 2, default: 0, null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["tweet_owner_id"], name: "index_favorited_owners_on_tweet_owner_id"
-    t.index ["user_id", "tweet_owner_id"], name: "index_favorited_owners_on_user_id_and_tweet_owner_id"
-    t.index ["user_id"], name: "index_favorited_owners_on_user_id"
-  end
-
   create_table "favorited_tweets", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "tweet_id", null: false
@@ -36,22 +25,8 @@ ActiveRecord::Schema.define(version: 2018_07_28_145234) do
     t.index ["user_id"], name: "index_favorited_tweets_on_user_id"
   end
 
-  create_table "favorited_tweets_tags", force: :cascade do |t|
-    t.bigint "favorited_tweet_id"
-    t.bigint "tag_id"
-    t.index ["favorited_tweet_id", "tag_id"], name: "index_favorited_tweets_tags_on_favorited_tweet_id_and_tag_id", unique: true
-    t.index ["favorited_tweet_id"], name: "index_favorited_tweets_tags_on_favorited_tweet_id"
-    t.index ["tag_id"], name: "index_favorited_tweets_tags_on_tag_id"
-  end
-
   create_table "tags", force: :cascade do |t|
     t.string "title", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "tweet_owners", force: :cascade do |t|
-    t.string "twitter_identifier", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -63,6 +38,14 @@ ActiveRecord::Schema.define(version: 2018_07_28_145234) do
     t.datetime "updated_at", null: false
     t.index ["tweet_identifier"], name: "index_tweets_on_tweet_identifier", unique: true
     t.index ["tweet_owner_id"], name: "index_tweets_on_tweet_owner_id"
+  end
+
+  create_table "tweets_tags", force: :cascade do |t|
+    t.bigint "tweet_id"
+    t.bigint "tag_id"
+    t.index ["tag_id"], name: "index_tweets_tags_on_tag_id"
+    t.index ["tweet_id", "tag_id"], name: "index_tweets_tags_on_tweet_id_and_tag_id", unique: true
+    t.index ["tweet_id"], name: "index_tweets_tags_on_tweet_id"
   end
 
   create_table "users", force: :cascade do |t|
