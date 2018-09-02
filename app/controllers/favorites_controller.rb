8e9b22efa3c302ca2f28a06470b81e9351717440
colorbox <colorbox222@gmail.com>
2018-09-02 22:12:04 +0900

    Fix tag on index

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,5 +1,5 @@
 class FavoritesController < ApplicationController
   def index
-    @favorited_tweets = current_user.favorited_tweets.includes(:tweet, :tags).page(params[:page]) if current_user
+    @favorited_tweets = current_user.favorited_tweets.includes(:tweet).page(params[:page]) if current_user
   end
 end
